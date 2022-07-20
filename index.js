ea9a724ff61ce55c599798da81e385eab58c996d
JamesB <James.buchmann@gmail.com>
2022-07-20 18:28:12 -0500

    reinstalled inquirer and added the first array of qs

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const firstQuestion = [
+{
+    type: 'list',
+    name: 'firstQuestion',
+    message: 'How many members are in your team?',
+    choices: [
+        1, 2, 3, 4, 5,
+    ]
+},
+]
+
+inquirer.prompt(firstQuestion).then((answer) => {
+    console.log(answer);
+})
