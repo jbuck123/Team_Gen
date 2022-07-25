4719ae76c998f9e946338df8911af5f32d38da1c
JamesB <James.buchmann@gmail.com>
2022-07-24 21:15:54 -0500

    fixed the errors in my code

--- index.js
+++ index.js
@@ -2,111 +2,102 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
-const Intern = require("./lib/intern")
-
-
-
+const Intern = require("./lib/intern");
 
 let array = [];
 
 // QUESTIONS
 
 const firstArray = [
-    {
-      type: "input",
-      name: "managerName",
-      message: "Please enter the manager's name.",
-      //validating input... if this returns false, ends the app.
-      validate: nameInput => {
-          if (nameInput) {
-              return true;
-          } else {
-              console.log("Please enter a valid name")
-              return false;
-          }
+  {
+    type: "input",
+    name: "managerName",
+    message: "Please enter the manager's name.",
+    //validating input... if this returns false, ends the app.
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a valid name");
+        return false;
       }
     },
-    {
-      type: "input",
-      name: "managerID",
-      message: "Please enter the manager ID number.",
-      //validate ID .. make sure it is a number
-      validate: id_input => {
-          if (isNaN(id_input)) {
-              console.log('please enter a valid ID number');
-              return false;
-          } else {
-              return true;
-          }
+  },
+  {
+    type: "input",
+    name: "managerID",
+    message: "Please enter the manager ID number.",
+    //validate ID .. make sure it is a number
+    validate: (id_input) => {
+      if (isNaN(id_input)) {
+        console.log("please enter a valid ID number");
+        return false;
+      } else {
+        return true;
       }
     },
-    {
-      type: "input",
-      name: "managerEmail",
-      message: "please enter the manager email.",
-      // i cant figure out how to 
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "please enter the manager email.",
+    // i cant figure out how to
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "please enter the manager's office number.",
+    validate: (id_input) => {
+      if (isNaN(id_input)) {
+        console.log("please enter a valid ID number");
+        return false;
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "please enter the manager's office number.",
-      validate: id_input => {
-        if (isNaN(id_input)) {
-            console.log('please enter a valid ID number');
-            return false;
-        } else {
-            return true;
-        }
-    }
-    },
-    {
-      type: "list",
-      name: "continue",
-      message: "do you wish to add more to the squad?",
-      choices: ["yes", "no"],
-    },
-  ];
-
-
-
+  },
+  {
+    type: "list",
+    name: "continue",
+    message: "do you wish to add more to the squad?",
+    choices: ["yes", "no"],
+  },
+];
 
 // 2nd array
 
-
-
-
 const secondArray = [
- {
-        type: "list",
-        Name: "role",
-        message: "Do you want to add an intern or an engineer",
-        choices: ["Intern", "Engineer"]
-},
+  {
+    type: "list",
+    name: "role",
+    message: "Do you want to add an intern or an engineer",
+    choices: ["Intern", "Engineer"],
+  },
   {
     type: "input",
     name: "name",
     message: "Please enter their name.",
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log("Please enter a valid name")
-            return false;
-        }
-    }
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a valid name");
+        return false;
+      }
+    },
   },
   {
     type: "input",
     name: "id",
     message: "Please enter the their ID.",
-    validate: id_input => {
-        if (isNaN(id_input)) {
-            console.log('please enter a valid ID number');
-            return false;
-        } else {
-            return true;
-        }
-    }
+    validate: (id_input) => {
+      if (isNaN(id_input)) {
+        console.log("please enter a valid ID number");
+        return false;
+      } else {
+        return true;
+      }
+    },
   },
   {
     type: "input",
@@ -117,111 +108,163 @@ const secondArray = [
     type: "input",
     name: "github",
     message: "Please enter the engineer's Github username.",
-   
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log("Please enter a valid name")
-            return false;
-        }
-    }
+
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter a valid name");
+        return false;
+      }
+    },
   },
   {
-    name: 'school',
-    message: 'where does the intern attend school?',
-  
+    type: "input",
+    name: "school",
+    message: "where does the intern attend school?",
   },
-
+  {
+      type: "list",
+      name: "continue",
+      message: "do you want to continue?",
+      choices: ["yes", "no"]
+  }
 ];
 
+// ContinueArray = [
+//   {
+//     type: "list",
+//     name: "continue",
+//     message: "Do you want to add more to your squad?",
+//     choices: ["yes", "no"],
+//   },
+// ];
 
-ContinueArray = [
-    {
-        type: "list",
-        name: "continue",
-        message: "Do you want to add more to your squad?",
-        choices: ["yes", "no"]
-      },
-]
-
-
-
-
-//FUNCTION STARTS 
-
-
+//FUNCTION STARTS
 
 //this starts with the first question which is who is manager at Default.
 
 inquirer.prompt(firstArray).then((managerData) => {
-  array.push(managerData)
-  console.log(array)
+// let employee = new Manager( 
+    
+//     managerData.name,
+//     managerData.id,
+//     managerData.email,
+//     managerData.officeNumber
+
+// )
+
   if (managerData.continue === "yes") {
-   addEmployee()
-  } else {
-    // displayTeam()
+    array.push(managerData);
+    console.log(array);
+    addEmployee();
+  } else if (managerData.continue === "no"){
+     
+    const printManager = displayManager(managerData);
+    fs.writeFile("output.html", printManager, (err) => {
+        err ? console.log(err): console.log("its working")
+    })
   }
 });
 
-function addEmployee(){
+function addEmployee() {
+  console.log("EMPLOYEE GENERATOR");
+   inquirer.prompt(secondArray).then((employeeData) => {
+     
+    let employee;
+    if (employeeData.role == "Intern") {
+        console.log()
+      employee = new Intern(
+        employeeData.name,
+        employeeData.id,
+        employeeData.email,
+        employeeData.school
+        
+      );
+      console.log(employee.Intern);
+      employee.printIntern();
+    } if (employeeData.role == "Engineer") {
+      employee = new Engineer(
+        employeeData.name,
+        employeeData.id,
+        employeeData.email,
+        employeeData.github
+      );
+      console.log(employee);
+      employee.printEngineer();
+    }
+    array.push(employee);
+    
 
-    console.log('EMPLOYEE GENERATOR')
-    return inquirer.prompt(secondArray)
-    .then((employeeData) => {
-        let employee
-        if ( employeeData.role === "Engineer"){
-            employee = new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school)
-            console.log(employee);
-        } if (employeeData.role === "Intern"){
-            employee = new Engineer(employeeData.name ,employeeData.id , employeeData.email, employeeData.github, )
-            console.log(employee);
-        }
-    })
+    console.log(array[1].getRole() === "Intern");
+    // this how you can check the role
+
+    if (employeeData.continue === "yes"){
+        addEmployee();
+        console.log(array)
+    } else {
+        printEmployee()
+        
+    }
+  });
+}
+
+function printEmployee(){
+    console.log("work in prog")
+    console.log(array)
+    // grab array
+  
+    // 
+    //loop through array 
+    // 
+}
+
+
+function displayManager(managerData){
+return `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Assets/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Generator</title>
+</head>
+<body>
+<header class="bg-secondary">
+    <h1 class="display-3 text-center text-danger">My Squad</h1> 
+    <h6 class="display-6 text-center text-danger">By: James Buchmann</h6>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <div class="card mx-auto" style="width: 18rem">
+                <h5 class="card-header">${managerData.managerName}<br /><br />Manager</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${managerData.managerID}</li>
+                    <li class="list-group-item">${managerData.managerEmail}</li>
+                    <li class="list-group-item">${managerData.officeNumber}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
+`
+    
 
 }
-    
-  
 
+// if (continue === "yes"){
+//     addEmployee()
 
-
-
-
-
-
-
-
-
-
-
-        // if (continue === "yes"){
-        //     addEmployee()
-
-        // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// }
 
 // function menuFunc() {
 //   inquirer
@@ -248,24 +291,23 @@ function addEmployee(){
 
 //       array.push(answer)
 //       console.log(answer)
-      
+
 //       if (answer.continue === "yes") {
 //         menuFunc();
 //       } else {
 //           displaySquad(array)
 //       }
-      
+
 //       })
-      
-      // .then chain this in the future to get better s
-  
+
+// .then chain this in the future to get better s
+
 // }
 // function addIntern() {
 //     inquirer.prompt(internQuestions).then((answer) => {
 //         console.log(answer);
 //         array.push(answer)
 //         console.log(array)
-       
 
 //         if (answer.continue === "yes") {
 //             menuFunc();
@@ -273,35 +315,20 @@ function addEmployee(){
 //               displaySquad(array)
 //           }
 //       });
-//     //.then chain this with classes(in the future).... use more advanced coding 
+//     //.then chain this with classes(in the future).... use more advanced coding
 // }
 
-// print array to html 
+// print array to html
 // create the team
 
 // function displaySquad(array){
 // const pageContent = JSON.stringify(array)
 // console.log("final array" + pageContent)
 
-
 // // fs.writeFile("index.html", pageContent, (err) => {
 // //     err ? console.log(err): console.log('its working')
 // // })
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const firstQuestion = [
 // {
