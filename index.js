8eb43d1d0b6455e173496e60635812f39941f79a
JamesB <James.buchmann@gmail.com>
2022-07-25 23:22:58 -0500

    the code is almost in its final product
    things that are need to improve. clickable links that send to email. that is easy enough

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // link to page creation 
-// const generateHTML = require('./src/generateHTML');
+const generateHTML = require('./src/generateHTML');
 
 
 
@@ -7,11 +7,15 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-//team profiles 
+
+//team profiles
 const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
-const Employee = require("./lib/employee");
+// const Employee = require("./lib/employee");
+const path = require("path")
+const out_dir = path.resolve(__dirname, "output");
+const output = path.join(out_dir, "team.html");
 
 //declaring the array
 let array = [];
@@ -186,7 +190,8 @@ function addEmployee() {
         employeeData.school
         
       );
-      console.log(employee);
+      array.push(employee)
+     console.log(array)
     //   employee.printIntern();
     } if (employeeData.role == "Engineer") {
         console.log("new Engineer")
@@ -196,10 +201,12 @@ function addEmployee() {
         employeeData.email,
         employeeData.github
       );
-      console.log(employee);
+
+      array.push(employee);
+      console.log(array)
     //   employee.printEngineer();
     }
-    array.push(employee);
+   
     
 
     // console.log(array[1].getRole() === "Intern");
@@ -209,16 +216,16 @@ function addEmployee() {
         addEmployee();
         console.log(array)
     } else {
-        htmlBuilder()
+      fileWriter();
         
     }
   });
 }
 
-function htmlBuilder () {
+function fileWriter () {
   console.log("please generate a team bro")
 
-  fs.writeFile("output.html", generateHTML(array), "UTF-8")
+  fs.writeFileSync(output, generateHTML(array), "UTF-8")
 
 }
 
@@ -260,115 +267,113 @@ return `
 </html>
 
 `
-    
-
 }
 
 
-var generateHTML = team => {
+// var generateHTML = team => {
   //create the manager card
-  const generateManager =  function(manager)  {
-    return `
-<div class="container">
-    <div class="row">
-        <div class="col-1">
-            <div class="card mx-auto" style="width: 18rem">
-                <h5 class="card-header">${manager.getName()}<br /><br />${manager.getRole()}</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${manager.getID()}</li>
-                    <li class="list-group-item">${manager.getEmail()}</li>
-                    <li class="list-group-item">${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>`;
-  };
+//  const generateManager =  function(manager)  {
+//     return `
+// <div class="container">
+//     <div class="row">
+//         <div class="col-1">
+//             <div class="card mx-auto" style="width: 18rem">
+//                 <h5 class="card-header">${manager.getName()}<br /><br />${manager.getRole()}</h5>
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">${manager.getID()}</li>
+//                     <li class="list-group-item">${manager.getEmail()}</li>
+//                     <li class="list-group-item">${manager.getOfficeNumber()}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </div>
+// </div>`;
+//   };
 
-  // create Engineer card
-  const generateEngineer = function (engineer) {
-    return `
-<div class="container">
-    <div class="row">
-        <div class="col-1">
-            <div class="card mx-auto" style="width: 18rem">
-                <h5 class="card-header">${engineer.getName()}<br /><br />${manager.getRole()}</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${engineer.getID()}</li>
-                    <li class="list-group-item">${engineer.getEmail()}</li>
-                    <li class="list-group-item">${engineer.getGithub()}</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>`;
-  };
-  // create the intern card
-  const generateIntern = function (intern) {
-    return `
-<div class="container">
-    <div class="row">
-        <div class="col-1">
-            <div class="card mx-auto" style="width: 18rem">
-                <h5 class="card-header">${intern.getName()}<br /><br />${intern.getRole()}</h5>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${intern.getID()}</li>
-                    <li class="list-group-item">${intern.getEmail()}</li>
-                    <li class="list-group-item">${intern.getSchool()}</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>`;
-  };
+//   // create Engineer card
+//   const generateEngineer = function (engineer) {
+//     return `
+// <div class="container">
+//     <div class="row">
+//         <div class="col-1">
+//             <div class="card mx-auto" style="width: 18rem">
+//                 <h5 class="card-header">${engineer.getName()}<br /><br />${manager.getRole()}</h5>
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">${engineer.getID()}</li>
+//                     <li class="list-group-item">${engineer.getEmail()}</li>
+//                     <li class="list-group-item">${engineer.getGithub()}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </div>
+// </div>`;
+//   };
+//   // create the intern card
+//   const generateIntern = function (intern) {
+//     return `
+// <div class="container">
+//     <div class="row">
+//         <div class="col-1">
+//             <div class="card mx-auto" style="width: 18rem">
+//                 <h5 class="card-header">${intern.getName()}<br /><br />${intern.getRole()}</h5>
+//                 <ul class="list-group list-group-flush">
+//                     <li class="list-group-item">${intern.getID()}</li>
+//                     <li class="list-group-item">${intern.getEmail()}</li>
+//                     <li class="list-group-item">${intern.getSchool()}</li>
+//                 </ul>
+//             </div>
+//         </div>
+//     </div>
+// </div>`;
+//   };
 
-  const page = [];
+//   const page = [];
 
-  page.push(
-    team
-      .filter((employee) => employee.getRole() === "Manager")
-      .map((manager) => generateManager(manager))
-  );
-  page.push(
-    team
-      .filter((employee) => employee.getRole() === "Engineer")
-      .map((engineer) => generateEngineer(engineer))
-      .join("")
-  );
-  page.push(
-    team
-      .filter((employee) => employee.getRole() === "Intern")
-      .map((intern) => generateIntern(intern))
-      .join("")
-  );
+//   page.push(
+//     team
+//       .filter((employee) => employee.getRole() === "Manager")
+//       .map((manager) => generateManager(manager))
+//   );
+//   page.push(
+//     team
+//       .filter((employee) => employee.getRole() === "Engineer")
+//       .map((engineer) => generateEngineer(engineer))
+//       .join("")
+//   );
+//   page.push(
+//     team
+//       .filter((employee) => employee.getRole() === "Intern")
+//       .map((intern) => generateIntern(intern))
+//       .join("")
+//   );
 
-  return html.join("");
-};
+//   return page.join("");
+// };
 
-// exporting this function to generate the html
+// // exporting this function to generate the html
 
-module.export = (team) => {
-  return `
+// module.exports = (team) => {
+//   return `
     
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Assets/styles.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Team Generator</title>
-</head>
-<body>
-<header class="bg-secondary">
-    <h1 class="display-3 text-center text-danger">My Squad</h1> 
-    <h6 class="display-6 text-center text-danger">By: James Buchmann</h6>
-</header>
-<div>
-${generateHTML(team)}
-</div>
-</body>
-</html>
-    `;
-};
+//     <!DOCTYPE html> 
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="./Assets/styles.css">
+//     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+//     <title>Team Generator</title>
+// </head>
+// <body>
+// <header class="bg-secondary">
+//     <h1 class="display-3 text-center text-danger">My Squad</h1> 
+//     <h6 class="display-6 text-center text-danger">By: James Buchmann</h6>
+// </header>
+// <div>
+// ${generateHTML(team)}
+// </div>
+// </body>
+// </html>
+//     `;
+// };
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,107 +1,107 @@
-// function generateHTML(team) {
-//   //create the manager card
-//   const generateManager =  function(manager)  {
-//     return `
-// <div class="container">
-//     <div class="row">
-//         <div class="col-1">
-//             <div class="card mx-auto" style="width: 18rem">
-//                 <h5 class="card-header">${manager.getName()}<br /><br />${manager.getRole()}</h5>
-//                 <ul class="list-group list-group-flush">
-//                     <li class="list-group-item">${manager.getID()}</li>
-//                     <li class="list-group-item">${manager.getEmail()}</li>
-//                     <li class="list-group-item">${manager.getOfficeNumber()}</li>
-//                 </ul>
-//             </div>
-//         </div>
-//     </div>
-// </div>`;
-//   };
+const generateHTML = array => {
+  //create the manager card
+  const generateManager =  manager => {
+    return `
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <div class="card mx-auto" style="width: 18rem">
+                <h5 class="card-header">${manager.getName()}<br /><br />${manager.getRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${manager.getID()}</li>
+                    <li class="list-group-item">${manager.getEmail()}</li>
+                    <li class="list-group-item">${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`;
+  };
 
-//   // create Engineer card
-//   const generateEngineer = function (engineer) {
-//     return `
-// <div class="container">
-//     <div class="row">
-//         <div class="col-1">
-//             <div class="card mx-auto" style="width: 18rem">
-//                 <h5 class="card-header">${engineer.getName()}<br /><br />${manager.getRole()}</h5>
-//                 <ul class="list-group list-group-flush">
-//                     <li class="list-group-item">${engineer.getID()}</li>
-//                     <li class="list-group-item">${engineer.getEmail()}</li>
-//                     <li class="list-group-item">${engineer.getGithub()}</li>
-//                 </ul>
-//             </div>
-//         </div>
-//     </div>
-// </div>`;
-//   };
-//   // create the intern card
-//   const generateIntern = function (intern) {
-//     return `
-// <div class="container">
-//     <div class="row">
-//         <div class="col-1">
-//             <div class="card mx-auto" style="width: 18rem">
-//                 <h5 class="card-header">${intern.getName()}<br /><br />${intern.getRole()}</h5>
-//                 <ul class="list-group list-group-flush">
-//                     <li class="list-group-item">${intern.getID()}</li>
-//                     <li class="list-group-item">${intern.getEmail()}</li>
-//                     <li class="list-group-item">${intern.getSchool()}</li>
-//                 </ul>
-//             </div>
-//         </div>
-//     </div>
-// </div>`;
-//   };
+  // create Engineer card
+  const generateEngineer =  engineer => {
+    return `
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <div class="card mx-auto" style="width: 18rem">
+                <h5 class="card-header">${engineer.getName()}<br /><br />${engineer.getRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${engineer.getID()}</li>
+                    <li class="list-group-item">${engineer.getEmail()}</li>
+                    <li class="list-group-item">${engineer.getGithub()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`;
+  };
+  // create the intern card
+  const generateIntern = intern => {
+    return `
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <div class="card mx-auto" style="width: 18rem">
+                <h5 class="card-header">${intern.getName()}<br /><br />${intern.getRole()}</h5>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${intern.getID()}</li>
+                    <li class="list-group-item">${intern.getEmail()}</li>
+                    <li class="list-group-item">${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>`;
+  };
 
-//   const page = [];
+  const page = [];
 
-//   page.push(
-//     team
-//       .filter((employee) => employee.getRole() === "Manager")
-//       .map((manager) => generateManager(manager))
-//   );
-//   page.push(
-//     team
-//       .filter((employee) => employee.getRole() === "Engineer")
-//       .map((engineer) => generateEngineer(engineer))
-//       .join("")
-//   );
-//   page.push(
-//     team
-//       .filter((employee) => employee.getRole() === "Intern")
-//       .map((intern) => generateIntern(intern))
-//       .join("")
-//   );
+  page.push(
+    array
+      .filter((manager) => manager.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  page.push(
+    array
+      .filter((engineer) => engineer.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  page.push(
+    array
+      .filter((intern) => intern.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
 
-//   return html.join("");
-// };
+  return page.join("");
+};
 
-// // exporting this function to generate the html
+// exporting this function to generate the html
 
-// module.export = (team) => {
-//   return `
+module.exports = (team) => {
+  return `
     
-//     <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <link rel="stylesheet" href="./Assets/styles.css">
-//     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-//     <title>Team Generator</title>
-// </head>
-// <body>
-// <header class="bg-secondary">
-//     <h1 class="display-3 text-center text-danger">My Squad</h1> 
-//     <h6 class="display-6 text-center text-danger">By: James Buchmann</h6>
-// </header>
-// <div>
-// ${generateTeam(team)}
-// </div>
-// </body>
-// </html>
-//     `;
-// };
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Assets/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Team Generator</title>
+</head>
+<body>
+<header class="bg-secondary">
+    <h1 class="display-3 text-center text-danger">My Squad</h1> 
+    <h6 class="display-6 text-center text-danger">By: James Buchmann</h6>
+</header>
+<div>
+${generateHTML(team)}
+</div>
+</body>
+</html>
+    `
+};
